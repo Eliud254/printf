f4eff8b5f27cc9141b168b2fddb3d9f8b7c96bc0
abel <abelamare3022@gmail.com>
2023-09-05 02:54:30 +0300

    h

--- main.h
+++ main.h
@@ -5,33 +5,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <stdio.h>
+#include <stdarg.h>
 
-/* Function prototypes */
-int _putchar(char c);
-int _printf(const char *format, ...);
-int printf_percent(void);
-int printf_char(va_list args);
-int printf_string(va_list args);
-int printf_int(va_list args);
-int printf_unsigned(va_list args);
-int printf_oct(va_list args);
-int printf_hex(va_list args);
-int printf_HEX(va_list args);
-int printf_pointer(va_list args);
-void print_buffer(char buffer[], int *buff_ind);
-int handle_print(const char *format, int *i, va_list args,
-char buffer[], int flags, int width, int precision, int size);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list args);
-int get_precision(const char *format, int *i, va_list args);
-int get_size(const char *format, int *i);
-int _putchar(char c);
-int _printf(const char *format, ...);
-
-/********************second try**************/
-
-int select(char c, va_list args);
-
+#define BUFFER_SIZE 1024
 
 #endif /* MAIN_H */
-
